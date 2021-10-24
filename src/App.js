2985dc753810711fc6ae9a6bff2bc54c3d37adfe
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-10-24 11:38:38 -0400

    module tells me to commit even though it doesn't tell me what the app should look like right now so I have no idea if what I have is correct

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React from 'react'; // this line was not supplied, but the module (20.1.5) shows it in App.js
+import React, { useState } from 'react'; // this line was not supplied, but the module (20.1.5) shows it in App.js
 // import logo from './logo.svg'; 20.1.5 does not show this line in App.js
 // import './App.css';20.1.5 does not show this line in App.js
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
-  return (
-    <div>
-        <Nav></Nav>
-        <main>
-            <About></About>
-        </main>
-    </div>
-  );
+    const [categories] = useState([
+        {
+            name: "commercial",
+            description: "Photos of grocery stores, food trucks, and other commercial projects"
+        },
+        {
+            name: "portraits",
+            description: "Portraits of people in my life"
+        },
+        {
+            name: "food",
+            description: "Delicious delicacies"
+        },
+        {
+            name: "landscape",
+            description: "Fields, farmhouses, waterfalls, and the beauty of nature"
+        }
+    ]);
+    const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+    return (
+        <div>
+            <Nav
+                vategories={categories}
+                setCurrentCategory={setCurrentCategory}
+                currentCategory={currentCategory}
+            ></Nav>
+            <main>
+                <Gallery></Gallery>
+                <About></About>
+            </main>
+        </div>
+    );
 }
 
 export default App;
